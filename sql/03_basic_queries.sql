25cb63705161dcf6acff909cd3790acecee68c66
Aleksandra Brząkowska <krola2417@gmail.com>
2026-02-01 16:59:33 +0100

    Create 03_basic_queries.sql

---
+++ sql/03_basic_queries.sql
@@ -0,0 +1,16 @@
+-- show all customers
+SELECT * 
+FROM customers;
+
+-- count customers
+SELECT COUNT(*) as 'Customers'
+FROM customers;
+
+-- average age
+SELECT AVG(age) 'Average_age' 
+FROM customers;
+
+-- customers from North region
+SELECT *
+FROM customers
+WHERE region = 'North';
